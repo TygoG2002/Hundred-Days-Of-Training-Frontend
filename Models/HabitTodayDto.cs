a32d4f2d2e265fc8519a58d2040b4c4ea5d15845
TygoG2002 <Tygo@tygodev.com>
2026-01-21 16:56:59 +0100

    added modal for habits

--- Models/HabitTodayDto.cs
+++ Models/HabitTodayDto.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int TargetValue { get; set; }
+        public int? TargetValue { get; set; }
 
-
+        public bool IsValueBased { get; set; }
         public int TodayValue { get; set; }
         public bool TodayCompleted { get; set; }
     }
